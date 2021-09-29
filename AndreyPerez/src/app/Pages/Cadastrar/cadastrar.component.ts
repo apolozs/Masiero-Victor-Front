16806ebc1a7044b36de24b856be20fb7112297e4
Perez <victor.perezsilva@hotmail.com>
2021-09-29 20:18:55 -0300

    Criação da component

---
+++ AndreyPerez/src/app/Pages/Cadastrar/cadastrar.component.ts
@@ -0,0 +1,14 @@
+// import { Pessoa } from "src/app/models/Pessoa";
+import { Component, OnInit } from "@angular/core";
+// import { PessoaService } from "../../services/quarto.service";
+
+@Component({
+    selector: "app-home",
+    templateUrl: "./cadastrar.component.html",
+    styleUrls: ["./cadastrar.component.css"],
+})
+export class PessoaComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
---
+++ AndreyPerez/src/app/app.routing.ts
@@ -0,0 +1,8 @@
+import { CadastrarComponent } from "./Pages/Cadastrar/cadastrar.component";
+import { Routes } from "@angular/router";
+
+const APP_ROUTES: Routes = [{ path: "", component: CadastrarComponent 
+    {
+        { path: 'Nome', component:CadastrarComponent},
+    }
+}];
